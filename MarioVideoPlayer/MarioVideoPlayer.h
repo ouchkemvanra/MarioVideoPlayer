991a7454f729c04a491d29ba8c1989ed03a8186b
kemvanra ouch <ouchkemvanra1@gmail.com>
2021-10-27 14:40:37 +0700

    Initial Commit

---
+++ MarioVideoPlayer/MarioVideoPlayer.h
@@ -0,0 +1,18 @@
+//
+//  MarioVideoPlayer.h
+//  MarioVideoPlayer
+//
+//  Created by Ouch Kemvanra on 10/27/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MarioVideoPlayer.
+FOUNDATION_EXPORT double MarioVideoPlayerVersionNumber;
+
+//! Project version string for MarioVideoPlayer.
+FOUNDATION_EXPORT const unsigned char MarioVideoPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarioVideoPlayer/PublicHeader.h>
+
+
